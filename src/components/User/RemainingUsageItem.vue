327117e2bfce10369eed6ac20f7e5f4eaff21ae9
Furkan <frknsprnl@hotmail.com>
2021-11-19 23:44:15 +0300

    added remainingusageitem.vue

---
+++ src/components/User/RemainingUsageItem.vue
@@ -0,0 +1,59 @@
+<template>
+        <section class="vh-100 gradient-custom">
+      <div class="container py-5 h-100">
+        <div class="row justify-content-center align-items-center h-100">
+          <div class="col-6 col col-7">
+            <div
+              class="card shadow-2-strong card-registration"
+              style="border-radius: 15px"
+            >
+              <figure class="text-center mt-3">
+                <blockquote class="blockquote">
+                  <h2>Paket Kalan Sorgulama</h2>
+                </blockquote>
+              </figure>
+              <div class="d-flex justify-content-center">
+                <div class="card text-center w-25">
+                  <div class="card-header">SMS</div>
+                  <div class="card-body">
+                    <h6 class="card-title">
+                      <i class="bi bi-envelope"></i> Kalan Sms
+                    </h6>
+                    <span class="card-text"> Kalan % </span>
+                  </div>
+                  <div class="card-footer text-muted"></div>
+                </div>
+              </div>
+              <br />
+              <div class="d-flex justify-content-center">
+                <div class="card text-center w-25">
+                  <div class="card-header">İnternet</div>
+                  <div class="card-body">
+                    <h6 class="card-title">
+                      <i class="bi bi-wifi"></i> Kalan İnternet
+                    </h6>
+                    <span class="card-text"> Kalan %</span>
+                  </div>
+                  <div class="card-footer text-muted"></div>
+                </div>
+              </div>
+              <br />
+              <div class="d-flex justify-content-center">
+                <div class="card text-center w-25">
+                  <div class="card-header">Arama</div>
+                  <div class="card-body">
+                    <h6 class="card-title">
+                      <i class="bi bi-telephone"></i> Kalan Arama
+                    </h6>
+                    <span class="card-text"> Kalan % </span>
+                  </div>
+                  <div class="card-footer text-muted"></div>
+                </div>
+              </div>
+              <br /><br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
