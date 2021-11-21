4934642730c22d7a257e8ff391686de0e3ae8027
Furkan <frknsprnl@hotmail.com>
2021-11-21 15:01:11 +0300

    profile page || remainingusageitem

--- src/components/User/RemainingUsageItem.vue
+++ src/components/User/RemainingUsageItem.vue
@@ -1,35 +1,60 @@
 <template>
-  <section class="container">
+  <section class="container border border-danger" id="max-container">
     <div class="container">
       <div class="d-flex justify-content-center align-items-center">
-        <div class="py-5">
-          <div class="card shadow-2-strong" style="border-radius: 15px">
-            <div class="row justify-content-md-center">
-              <div class="card text-center w-25 me-4">
-                <div class="card-header">SMS</div>
-                <div class="card-body">
-                  <h6 class="card-title"><i class="bi bi-envelope"></i> Kalan Sms</h6>
-                  <span class="card-text"> Kalan % </span>
-                </div>
-                <div class="card-footer text-muted"></div>
+        <div class="pt-5">
+          <div class="row">
+            <div class="card col-md-4">
+              <div class="card-title">
+                <h3 class="text-center pt-1">Dakika</h3>
+                <hr />
               </div>
-
-              <div class="card text-center w-25 me-4">
-                <div class="card-header">İnternet</div>
-                <div class="card-body">
-                  <h6 class="card-title"><i class="bi bi-wifi"></i> Kalan İnternet</h6>
-                  <span class="card-text"> Kalan %</span>
+              <div class="circle-wrap me-2">
+                <div class="circle">
+                  <div class="mask full">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="mask half">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="inside-circle">70%</div>
                 </div>
-                <div class="card-footer text-muted"></div>
               </div>
+            </div>
 
-              <div class="card text-center w-25 me-4">
-                <div class="card-header">Arama</div>
-                <div class="card-body">
-                  <h6 class="card-title"><i class="bi bi-telephone"></i> Kalan Arama</h6>
-                  <span class="card-text"> Kalan % </span>
+            <div class="card col-md-4">
+              <div class="card-title">
+                <h3 class="text-center pt-1 pe-2">İnternet</h3>
+                <hr />
+              </div>
+              <div class="circle-wrap me-2">
+                <div class="circle">
+                  <div class="mask full">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="mask half">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="inside-circle">70%</div>
                 </div>
-                <div class="card-footer text-muted"></div>
+              </div>
+            </div>
+
+            <div class="card col-md-4">
+              <div class="card-title">
+                <h3 class="text-center pt-1">SMS</h3>
+                <hr />
+              </div>
+              <div class="circle-wrap me-2">
+                <div class="circle">
+                  <div class="mask full">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="mask half">
+                    <div class="fill"></div>
+                  </div>
+                  <div class="inside-circle">70%</div>
+                </div>
               </div>
             </div>
           </div>
@@ -38,3 +63,58 @@
     </div>
   </section>
 </template>
+<style scoped>
+.circle-wrap {
+  margin: 20px;
+  width: 150px;
+  height: 150px;
+  background: #e6e2e7;
+  border-radius: 50%;
+}
+
+.circle-wrap .circle .mask,
+.circle-wrap .circle .fill {
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  border-radius: 50%;
+}
+
+.circle-wrap .circle .mask {
+  clip: rect(0px, 150px, 150px, 75px);
+}
+
+.circle-wrap .circle .mask .fill {
+  clip: rect(0px, 75px, 150px, 0px);
+  background-color: #9e00b1;
+}
+
+.circle-wrap .circle .mask.full,
+.circle-wrap .circle .fill {
+  animation: fill ease-in-out 3s;
+  transform: rotate(126deg);
+}
+
+@keyframes fill {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(126deg);
+  }
+}
+.circle-wrap .inside-circle {
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  background: #fff;
+  line-height: 130px;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 10px;
+  position: absolute;
+  z-index: 100;
+  font-weight: 700;
+  font-size: 2em;
+}
+</style>
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -29,29 +29,9 @@
       <deposit v-else-if="activeTab === 'Deposit'" />
       <password-reset v-else-if="activeTab === 'PasswordReset'" />
       <fatura-detay v-else-if="activeTab === 'FaturaDetay'" />
-      <div class="container d-flex justify-content-center" v-else-if="activeTab === 'UserPlan'">
+      <div class="container d-flex justify-content-start" v-else-if="activeTab === 'UserPlan'">
         <user-plan class="" />
         <remaining-usage-item class="" />
-
-        <!-- <div class="d-flex justify-content-center align-items-center">
-          <div class="card">
-            <div class="card-title">
-              <h3 class="text-center">Dakika</h3>
-              <hr />
-            </div>
-            <div class="circle-wrap me-2">
-              <div class="circle">
-                <div class="mask full">
-                  <div class="fill"></div>
-                </div>
-                <div class="mask half">
-                  <div class="fill"></div>
-                </div>
-                <div class="inside-circle">70%</div>
-              </div>
-            </div>
-          </div>
-        </div> -->
       </div>
     </div>
     <footer-bar />
@@ -102,59 +82,6 @@ export default {
 </script>
 
 <style scoped>
-.circle-wrap {
-  margin: 50px auto;
-  width: 150px;
-  height: 150px;
-  background: #e6e2e7;
-  border-radius: 50%;
-}
-
-.circle-wrap .circle .mask,
-.circle-wrap .circle .fill {
-  width: 150px;
-  height: 150px;
-  position: absolute;
-  border-radius: 50%;
-}
-
-.circle-wrap .circle .mask {
-  clip: rect(0px, 150px, 150px, 75px);
-}
-
-.circle-wrap .circle .mask .fill {
-  clip: rect(0px, 75px, 150px, 0px);
-  background-color: #9e00b1;
-}
-
-.circle-wrap .circle .mask.full,
-.circle-wrap .circle .fill {
-  animation: fill ease-in-out 3s;
-  transform: rotate(126deg);
-}
-
-@keyframes fill {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(126deg);
-  }
-}
-.circle-wrap .inside-circle {
-  width: 130px;
-  height: 130px;
-  border-radius: 50%;
-  background: #fff;
-  line-height: 130px;
-  text-align: center;
-  margin-top: 10px;
-  margin-left: 10px;
-  position: absolute;
-  z-index: 100;
-  font-weight: 700;
-  font-size: 2em;
-}
 .btn-lg {
   background-color: #503c6c;
   color: white;
