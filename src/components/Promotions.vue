7ac932ebf623a809dc1541b2eaefc35e358b4f14
Furkan <frknsprnl@hotmail.com>
2021-12-17 16:07:32 +0300

    promotions/faq/userdetails updated.

--- src/components/Promotions.vue
+++ src/components/Promotions.vue
@@ -1,54 +1,36 @@
 <template>
   <div class="container rounded bg-white mt-5 mb-5 d-flex justify-content-center">
-    <div class="row">
-      <div class="col-4">
-        <div class="card hover-div" style="width: 10rem">
-          <img src="../assets/promotion1.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">D&R'da Hediye Kitap!</h6>
-            <hr />
-            <p class="card-text" id="promotion-text">
-              22-28 Kasım haftası D&R’da geçerli Hediye Kitap ayrıcalığından yararlanan ilk 10.000
-              müşterimizden biri olmak için şifrenizi Paycell uygulamasından alabilirsiniz. Keyifli okumalar
-              dileriz.
-            </p>
-          </div>
+    <div class="row d-flex justify-content-center">
+      <div
+        class="card hover-div me-3"
+        style="width: 10rem"
+        v-for="promotions in promotion"
+        :key="promotions.id"
+      >
+        <img :src="require('@/assets/' + promotions.image)" class="card-img" alt="..." />
+        <div class="card-body">
+          <h6 class="text-center">
+            {{ promotions.promoName }}
+          </h6>
+          <hr />
+          <p class="card-text" id="promotion-text">{{ promotions.promoDesc }}</p>
         </div>
-      </div>
-      <div class="col-4">
-        <div class="card hover-div" style="width: 10rem">
-          <img src="../assets/promotion2.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">Kahve.com'da 50 TL Hediye</h6>
-            <hr />
-            <p class="card-text" id="promotion-text">
-              Kahve.com sitesinde geçerli 50 TL değerindeki indirim hediyenizden yararlanmak için Parcell
-              şifrenizi alabilir, alışveriş sırasında «hediye çeki» alanında kullanabilirsiniz.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card hover-div" style="width: 10rem">
-          <img src="../assets/promotion3.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">COVID-19 Testlerinde %30 indirim</h6>
-            <hr />
-            <p class="card-text" id="promotion-text">
-              Memorial’da yaptıracağınız COVID-19 PCR testi, IgM antikor testi, aşı sonrası IgG antikor testi
-              ve COVID-19 genetik yatkınlık testlerinden %30 indirim ile yararlanmak için şifrenizi
-              alabilirsiniz.
-            </p>
-          </div>
-        </div>
+
+        <button class="btn btn-primary mb-2">
+          {{ promotions.promoCode }}
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.text-center {
+  user-select: none;
+}
 #promotion-text {
   font-size: 14px;
+  user-select: none;
 }
 
 .hover-div {
@@ -73,7 +55,35 @@
   -ms-transform: translateY(-20px);
   transform: translateY(-20px);
   box-shadow: 0 22px 43px rgba(0, 0, 0, 0.32);
-  cursor: pointer;
+  cursor: auto;
   border-radius: 5px;
 }
 </style>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {
+    //
+  },
+  data() {
+    return {
+      // users: [],
+      promotion: {},
+    };
+  },
+  created() {
+    this.$appAxios
+      .get("https://parcell-backend.azure-api.net/v1/api/Promotions/GetPromoList")
+      .then((response) => {
+        console.log(response.data);
+        this.promotion = response.data;
+        console.log(this.promotion);
+      })
+      .catch((error) => {
+        console.log("There was an error" + error.response);
+      });
+  },
+};
+</script>
--- src/components/User/UserDetails.vue
+++ src/components/User/UserDetails.vue
@@ -5,7 +5,7 @@
         <div class="col-md-3 border-right">
           <div class="d-flex flex-column align-items-center mt-5 p-4">
             <img class="img-thumbnail rounded-circle mt-5" src="../../assets/userimage.jpg" />
-            <span class="mt-2" style="font-size: 1.2rem">Furkan Süpürenel</span>
+            <span class="mt-2" style="font-size: 1.2rem">{{ user.name }} {{ user.surname }}</span>
             <span class="mt-2" style="font-size: 1.2rem">999.99₺</span>
           </div>
         </div>
@@ -18,37 +18,43 @@
               <div class="row mt-3">
                 <div class="col-md-12">
                   <label class="labels"><i class="bi bi-people-fill"> Müşteri No.</i></label
-                  ><input type="text" class="form-control" value="1911012054" readonly />
+                  ><input type="text" class="form-control" v-model="user.id" disabled />
                 </div>
                 <div class="col-md-6">
                   <label class="labels"> <i class="bi bi-person-fill"> Ad</i></label
-                  ><input
-                    type="text"
-                    class="form-control"
-                    v-for="users in users"
-                    :key="users.Name"
-                    :users="users"
-                  />
+                  ><input type="text" class="form-control" v-model="user.name" disabled />
                 </div>
                 <div class="col-md-6">
                   <label class="labels"> <i class="bi bi-person-fill"> Soyad</i></label
-                  ><input type="text" class="form-control" />
+                  ><input type="text" class="form-control" v-model="user.surname" disabled />
                 </div>
               </div>
               <div class="row mt-3">
                 <div class="col-md-6">
                   <label class="labels"><i class="bi bi-gender-ambiguous"> Cinsiyet</i></label
-                  ><input type="text" class="form-control" placeholder="Erkek/Kadın" />
+                  ><input
+                    type="text"
+                    class="form-control"
+                    placeholder="Erkek/Kadın"
+                    v-model="user.gender"
+                    disabled
+                  />
                 </div>
                 <div class="col-md-6">
                   <label class="labels"><i class="bi bi-telephone-fill"> Telefon No.</i></label
-                  ><input type="text" class="form-control" />
+                  ><input type="text" class="form-control" v-model="user.phone" disabled />
                 </div>
               </div>
               <div class="row mt-3">
                 <div class="col-md-12">
                   <label class="labels"><i class="bi bi-envelope-open-fill"> Email</i> </label
-                  ><input type="text" class="form-control" placeholder="user@gmail.com" />
+                  ><input
+                    type="text"
+                    class="form-control"
+                    placeholder="user@gmail.com"
+                    v-model="mail"
+                    :disabled="disabled == 0"
+                  />
                 </div>
                 <div class="col-md-12">
                   <label class="labels"><i class="bi bi-geo-alt-fill"> Adres</i></label
@@ -57,22 +63,30 @@
                     class="form-control"
                     placeholder="adres"
                     value="Halikent,4, 32040, Merkez/Eskisehir, Turkey"
+                    :disabled="disabled == 0"
                   />
                 </div>
               </div>
               <div class="row mt-3">
                 <div class="col-md-6">
                   <label class="labels"><i class="bi bi-globe2"> Doğum Yeri</i></label
-                  ><input type="text" class="form-control" placeholder="Şehir" />
+                  ><input
+                    type="text"
+                    class="form-control"
+                    placeholder="Şehir"
+                    v-model="user.birthPlace"
+                    disabled
+                  />
                 </div>
                 <div class="col-md-6">
                   <label class="labels"><i class="bi bi-calendar-fill"> Doğum Tarihi</i></label
-                  ><input type="text" class="form-control" />
+                  ><input type="date" class="form-control" v-model="user.birthDate" disabled />
                 </div>
               </div>
               <div class="mt-5 d-flex justify-content-center">
-                <button class="btn btn-danger me-2" type="button">
-                  <i class="bi bi-pen me-1"> </i> <span>Profili Düzenle</span>
+                <button class="btn btn-danger me-2" type="button" @click="disabled = (disabled + 1) % 2">
+                  <i class="bi bi-pen me-1"> </i>
+                  <span>Profili Düzenle</span>
                 </button>
                 <button class="btn btn-primary profile-button" type="submit" @click="addToAPI">
                   <i class="bi bi-save me-1"> </i> <span>Profili Kaydet</span>
@@ -94,14 +108,23 @@ export default {
   },
   data() {
     return {
-      users: [],
+      // users: [],
+      mail: "frknsprnl@hotmail.com",
+      user: {},
+      disabled: 0,
     };
   },
+
   created() {
-    axios
-      .get("http://localhost:29418/api/users")
+    this.$appAxios
+      .get("/User/GetUserWithMail?mail=" + this.mail)
       .then((response) => {
-        this.users = response.data;
+        console.log(response.data);
+        //this.user = response.data;
+        let date = response.data.birthDate.slice(0, 10);
+        this.user = { ...response.data, birthDate: date };
+        console.log(date);
+        console.log(this.user);
       })
       .catch((error) => {
         console.log("There was an error" + error.response);
@@ -110,7 +133,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .img-round {
   border-radius: 50%;
 }
