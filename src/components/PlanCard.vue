b5cfb67bd18fb1ca1ca7e24136f3582030a06dd5
Furkan <frknsprnl@hotmail.com>
2021-12-18 16:05:55 +0300

    plan card | plan images updated. | azure/db

--- src/components/PlanCard.vue
+++ src/components/PlanCard.vue
@@ -1,61 +1,24 @@
 <template>
-  <div class="container rounded bg-white mt-5 mb-5 d-flex justify-content-center">
+  <div class="container mt-5 mb-5 d-flex justify-content-center">
     <div class="row">
-      <div class="col-4">
-        <div class="card p-2" style="width: 18rem">
-          <img src="../assets/plan1.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">Süper Fırsat 10GB +</h6>
-            <hr />
-            <p class="text-center" id="promotion-text">
-              Süper Fırsat 10GB paketiyle yüksek hızda interneti ve zengin içerikleri doya doya
-              yaşayabileceksiniz. Üstelik, her yöne 1000DK bol bol konuşma, 1000SMS, 10GB internet ile
-              iletişimin keyfini sürün.
-            </p>
-          </div>
-          <div class="d-flex justify-content-md-center">
-            <button class="btn btn-primary btn-lg mt-2">
-              <i class="bi bi-cart-plus-fill"></i>
-            </button>
-          </div>
+      <h6 class="d-flex justify-content-center">
+        Parcell paketleriyle yüksek hızda interneti ve zengin içerikleri doya doya yaşayabileceksiniz.
+      </h6>
+      <div class="card p-2 me-3 ms-4 mt-4" style="width: 18rem" v-for="plan in plan" :key="plan.id">
+        <img :src="require('@/assets/' + plan.image)" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h6 class="text-center">{{ plan.planName }}</h6>
+          <hr />
+          <p class="text-center" id="promotion-text">
+            {{ plan.planName }} paketiyle yüksek hızda interneti ve zengin içerikleri doya doya
+            yaşayabileceksiniz. Üstelik, her yöne {{ plan.minutes }} bol bol konuşma, {{ plan.sms }},
+            {{ plan.internet }} internet ile iletişimin keyfini sürün.
+          </p>
         </div>
-      </div>
-      <div class="col-4">
-        <div class="card p-2" style="width: 18rem">
-          <img src="../assets/plan2.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">Süper Fırsat 13GB+</h6>
-            <hr />
-            <p class="text-center" id="promotion-text">
-              Süper Fırsat 13GB+ paketiyle yüksek hızda interneti ve zengin içerikleri doya doya
-              yaşayabileceksiniz. Üstelik, her yöne 1000DK bol bol konuşma, 300SMS, 13GB internet ile
-              iletişimin keyfini sürün.
-            </p>
-          </div>
-          <div class="d-flex justify-content-md-center">
-            <button class="btn btn-primary btn-lg mt-2">
-              <i class="bi bi-cart-plus-fill"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card p-2" style="width: 18rem">
-          <img src="../assets/plan3.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h6 class="text-center">Süper Fırsat 20GB+</h6>
-            <hr />
-            <p class="text-center" id="promotion-text">
-              Süper Fırsat 20GB+ paketiyle yüksek hızda interneti ve zengin içerikleri doya doya
-              yaşayabileceksiniz. Üstelik, her yöne 1000DK bol bol konuşma, 300SMS, 20GB internet ile
-              iletişimin keyfini sürün.
-            </p>
-          </div>
-          <div class="d-flex justify-content-md-center">
-            <button class="btn btn-primary btn-lg mt-2">
-              <i class="bi bi-cart-plus-fill"></i>
-            </button>
-          </div>
+        <div class="d-flex justify-content-md-center">
+          <button class="btn btn-primary btn-lg">
+            <i class="bi bi-cart-plus-fill"></i>
+          </button>
         </div>
       </div>
     </div>
@@ -63,7 +26,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      plan: {},
+    };
+  },
+  created() {
+    this.$appAxios
+      .get("/Plan/GetPlans")
+      .then((response) => {
+        console.log(response.data);
+        this.plan = response.data;
+        console.log(this.plan);
+      })
+      .catch((error) => {
+        console.log("There was an error" + error.response);
+      });
+  },
+};
 </script>
 
 <style lang="sass" scoped>
