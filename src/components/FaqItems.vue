b3f8fb4cbeb6c00e7f078963bee02111f23d7063
Furkan <frknsprnl@hotmail.com>
2021-12-20 19:48:10 +0300

    FaqItems | Load Animation

--- src/components/FaqItems.vue
+++ src/components/FaqItems.vue
@@ -1,52 +1,64 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <h4>Sık Sorulan Sorular</h4>
+  <load-animation v-if="getLoadingStatus" />
+  <div v-if="!getLoadingStatus">
+    <h4 class="d-flex justify-content-center mt-3 mb-5">Sık Sorulan Sorular</h4>
+
+    <br />
+
+    <vue-collapsible-panel
+      class="user-select-none"
+      :expanded="false"
+      bg-color-body="#4c3a6e"
+      v-for="faq in faq"
+      :key="faq.id"
+    >
+      <template #title>
+        {{ faq.question }}
+      </template>
+      <template #content> {{ faq.answer }}</template>
+    </vue-collapsible-panel>
   </div>
-  <br />
-  <vue-collapsible-panel :expanded="false" bg-color-body="#4c3a6e" v-for="faq in faq" :key="faq.id">
-    <template #title>
-      {{ faq.question }}
-    </template>
-    <template #content> {{ faq.answer }}</template>
-  </vue-collapsible-panel>
 </template>
-
-<!--
-<script>
-import { defineComponent } from "@vue/composition-api";
-import VueCollapsiblePanel from "../components/VueCollapsiblePanel.vue";
-
-export default defineComponent({
-	setup() {},
-});
-</script>
--->
 
 <script>
 import VueCollapsiblePanel from "../components/VueCollapsiblePanel.vue";
 import axios from "axios";
+import LoadAnimation from "../components/Shared/LoadAnimation.vue";
 export default {
   components: {
     VueCollapsiblePanel,
+    LoadAnimation,
   },
 
   data() {
     return {
-      // users: [],
       faq: {},
+      isLoading: true,
     };
   },
-  created() {
-    this.$appAxios
-      .get("https://parcell-backend.azure-api.net/v1/api/Faq/GetFAQList")
-      .then((response) => {
-        console.log(response.data);
-        this.faq = response.data;
-        console.log(this.faq);
-      })
-      .catch((error) => {
-        console.log("There was an error" + error.response);
-      });
+  async created() {
+    this.isLoading = true;
+    await this.getData();
+    this.isLoading = false;
+  },
+  methods: {
+    async getData() {
+      await this.$appAxios
+        .get("https://parcell-backend.azure-api.net/v1/api/Faq/GetFAQList")
+        .then((response) => {
+          console.log(response.data);
+          this.faq = response.data;
+          console.log(this.faq);
+        })
+        .catch((error) => {
+          console.log("There was an error" + error.response);
+        });
+    },
+  },
+  computed: {
+    getLoadingStatus: function () {
+      return this.isLoading;
+    },
   },
 };
 </script>
