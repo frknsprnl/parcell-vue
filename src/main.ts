a9cbd5c28807c356f61c6aeee7fd0f3044135bd6
Muammer Yilmaz <muammercan68@gmail.com>
2021-12-25 01:05:29 +0300

    unused dependencies removed

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { appAxios } from './utils/appAxios'
-import Loading from "@/components/Shared/Loading.vue";
 import Toast from "vue-toastification";
-// Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 import { useToast } from "vue-toastification";
-import ConfirmationService from 'primevue/confirmationservice';
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import "@/../public/global.scss";
+
+
+const options = {
+    confirmButtonColor: '#4c3a6e',
+    cancelButtonColor: '#d33',
+};
+
+
 
 const toast = useToast();
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.component("Loading", {
-    color : "#4c3a6e",
-    size : "150 150"
-});
+
 app.use(Toast);
-app.use(ConfirmationService);
+
+app.use(VueSweetalert2,options);
 app.config.globalProperties.$appAxios = appAxios;
 app.config.globalProperties.$toast = toast;
-
 
 app.mount('#app');
