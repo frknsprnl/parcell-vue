c7a4446c63048900ab84fa9e461329075939c958
Furkan <frknsprnl@hotmail.com>
2021-12-26 17:59:08 +0300

    paymentpage remake

--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,97 +1,139 @@
 <template>
+  <navbar />
   <div class="container py-5">
-    <div class="py-5">
-      <div class="container">
-        <form class="row g-3 justify-content-center mb-2">
-          <div class="col-sm-6">
-            <button @click="goBack()" class="btn btn-secondary btn-md mb-3">Geri Dön</button>
-            <div class="row">
-              <h2 class="mb-3">Kart Bilgileri</h2>
+    <div class="card-list">
+      <VuePaycard :value-fields="valueFields" :inputFields="inputFields"> </VuePaycard>
+      <div class="card-form__inner">
+        <div class="card-input">
+          <label for="cardName" class="card-input__label">Kart Sahibi</label>
+          <input
+            type="text"
+            id="v-card-name"
+            title="Name"
+            data-card-field=""
+            autocomplete="off"
+            class="card-input__input"
+            v-model="valueFields.cardName"
+          />
+        </div>
+
+        <div class="card-input">
+          <label for="cardNumber" class="card-input__label">Kart Numarası</label>
+          <input
+            type="tel"
+            id="v-card-number"
+            title="Number"
+            data-card-field=""
+            v-model="valueFields.cardNumber"
+            autocomplete="off"
+            maxlength="19"
+            class="card-input__input"
+          />
+        </div>
+        <div class="card-form__row">
+          <div class="card-form__col">
+            <div class="card-form__group">
+              <label for="cardMonth" aria-label="Expiration Date" class="card-input__label"
+                >Son Kullanım Tarihi</label
+              >
+              <select
+                id="v-card-month"
+                aria-label="Card Month"
+                title="Month"
+                data-card-field=""
+                class="card-input__input -select me-3"
+                v-model="valueFields.cardMonth"
+              >
+                <option value="" disabled="disabled" selected="selected">Ay</option>
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+              <select
+                id="v-card-year"
+                aria-label="Card year"
+                title="Year"
+                data-card-field=""
+                class="card-input__input -select"
+                v-model="valueFields.cardYear"
+              >
+                <option value="" disabled="disabled" selected="selected">Yıl</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+                <option value="2026">2026</option>
+                <option value="2027">2027</option>
+                <option value="2028">2028</option>
+                <option value="2029">2029</option>
+                <option value="2030">2030</option>
+                <option value="2031">2031</option>
+                <option value="2032">2032</option>
+              </select>
             </div>
-            <label for="firstName" class="form-label">Kart Numarası</label>
-            <input type="tel" class="form-control" id="firstName" v-cardformat:formatCardNumber />
           </div>
-        </form>
-        <form class="row g-3 justify-content-center">
-          <div class="col-sm-2">
-            <label for="stateinf" class="form-label">Ay</label>
-            <select class="form-select form-select" aria-label=".form-select-sm example" required>
-              <option selected>Seçiniz</option>
-              <option value="1">01</option>
-              <option value="2">02</option>
-              <option value="3">03</option>
-              <option value="1">04</option>
-              <option value="2">05</option>
-              <option value="3">06</option>
-              <option value="1">07</option>
-              <option value="2">08</option>
-              <option value="3">09</option>
-              <option value="1">10</option>
-              <option value="2">11</option>
-              <option value="3">12</option>
-            </select>
-          </div>
-
-          <div class="col-sm-2">
-            <label for="stateinf" class="form-label">Yıl</label>
-            <select class="form-select form-select" aria-label=".form-select-sm example" required>
-              <option selected>Seçiniz</option>
-              <option value="1">2021</option>
-              <option value="2">2022</option>
-              <option value="3">2023</option>
-            </select>
-          </div>
-
-          <div class="col-sm-2">
-            <label for="CVV" class="form-label">CVV</label>
-            <input type="text" class="form-control" id="CVV" maxlength="3" required />
-          </div>
-        </form>
-        <div class="row justify-content-center mt-3">
-          <div class="col-sm-6">
-            <button class="btn btn-primary btn-md mb-4" id="btn-pay">Ödeme yap</button>
-            <vue-collapsible-panel-group accordion :baseColor="'#482c77'">
-              <vue-collapsible-panel :expanded="false">
-                <template #title> Sözleşmeler ve Formlar </template>
-                <template #content>
-                  <p>
-                    E TİCARET MESAFELİ SATIŞ SÖZLEŞMESİ Madde 1- Taraflar 1.1. Satıcı Adı: Özdisan Elektronik
-                    Sanayi ve Ticaret A.Ş. Adresi: DES Sanayi Sitesi 104 Sokak A07 Blok No: 54 - 56 Yukarı
-                    Dudullu / Ümraniye / İstanbul - Türkiye Telefon: +902164201882 MERSIS NO:
-                    0683-0033-7910-0011 E mail: info@ozdisan.com 1.2. Alıcı Adı – Soyadı: TCK. No: Adresi:
-                    Telefon: E-mail: Madde 2- Konu İşbu sözleşmenin konusu, ALICI’ nın SATICI’ ya ait
-                    www.ozdisan.com internet sitesinden elektronik ortamda siparişini yaptığı aşağıda
-                    nitelikleri ve satış ücreti belirtilen ürünün satışı ve teslimi ile ilgili olarak Türk
-                    Ticaret Kanunu, Borçlar Kanunu ve 6502 sayılı Tüketicilerin Korunması Hakkındaki Kanun ve
-                    Mesafeli Sözleşmelere Dair Yönetmelik hükümleri gereğince tarafların hak ve
-                    yükümlülüklerinin saptanmasıdır. Alıcı, satıcının isim, unvan, açık adres, telefon ve
-                    diğer erişim bilgileri, satışa konu malın temel nitelikleri, vergiler dahil olmak üzere
-                    satış fiyatı, ödeme şekli, teslimat koşulları ve masrafları vs. satışa konu mal ile ilgili
-                    tüm ön bilgiler ve “cayma” hakkının kullanılması ve bu hakkın nasıl kullanılacağı, şikayet
-                    ve itirazlarını iletebilecekleri resmi makamlar vs. konusunda açık, anlaşılır ve internet
-                    ortamına uygun şekilde satıcı tarafından bilgilendirildiğini, bu ön bilgileri elektronik
-                    ortamda teyit ettiğini ve sonrasında mal sipariş verdiğini is bu sözleşme hükümlerince
-                    kabul ve beyan eder. www.ozdisan.com sitesinde yer alan ön bilgilendirme ve alıcı
-                    tarafından verilen sipariş üzerine düzenlenen fatura is bu sözleşmenin ayrılmaz
-                    parçalarıdır. Madde 3- Sözleşme Konusu Ürün/Ödeme/Teslimat Bilgileri Elektronik ortamda
-                    alınan ürün/ürünlerin cinsi
-                  </p>
-                </template>
-              </vue-collapsible-panel>
-            </vue-collapsible-panel-group>
+          <div class="card-form__col -cvv">
+            <div class="card-input">
+              <label for="cardCvv" aria-label="Card CVV" class="card-input__label">CVV</label>
+              <input
+                type="tel"
+                title="CVV"
+                id="v-card-cvv"
+                maxlength="4"
+                data-card-field=""
+                autocomplete="off"
+                class="card-input__input"
+                v-model="valueFields.cardCvv"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <footer-bar />
 </template>
 
 <script>
-import VueCollapsiblePanelGroup from "@/components/VueCollapsiblePanelGroup.vue";
-import VueCollapsiblePanel from "@/components/VueCollapsiblePanel.vue";
+import FooterBar from "../components/Shared/FooterBar.vue";
+import Navbar from "../components/Shared/Navbar.vue";
 export default {
-  components: { VueCollapsiblePanelGroup, VueCollapsiblePanel },
-
+  components: { Navbar, FooterBar },
+  data: () => ({
+    valueFields: {
+      cardName: "",
+      cardNumber: "",
+      cardMonth: "",
+      cardYear: "",
+      cardCvv: "",
+    },
+    inputFields: {
+      cardNumber: "v-card-number",
+      cardName: "v-card-name",
+      cardMonth: "v-card-month",
+      cardYear: "v-card-year",
+      cardCvv: "v-card-cvv",
+    },
+    labels: {
+      cardName: "AD SOYAD",
+      cardHolder: "Kart Sahibi",
+      cardMonth: "MM",
+      cardYear: "YY",
+      cardExpires: "Son Kull. Tarih",
+      cardCvv: "CVV",
+    },
+  }),
   methods: {
     goBack() {
       this.$router.go(-1);
@@ -100,3 +142,84 @@ export default {
 };
 </script>
 
+<style scoped>
+.container {
+  margin-top: 90px;
+  margin-bottom: 90px;
+}
+#v-card-year {
+  max-width: 100px;
+}
+#v-card-month {
+  max-width: 100px;
+}
+.card-form__inner {
+  background: #fff;
+  box-shadow: 0px 30px 60px 0 rgb(90 116 148 / 40%);
+  border-radius: 10px;
+  padding: 45px 25px 25px 25px;
+}
+.card-input {
+  margin-bottom: 20px;
+}
+.card-input__label {
+  font-size: 14px;
+  margin-bottom: 5px;
+  font-weight: 500;
+  color: #1a3b5d;
+  width: 100%;
+  display: block;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.card-input__input {
+  width: 100%;
+  height: 50px;
+  border-radius: 5px;
+  box-shadow: none;
+  border: 1px solid #ced6e0;
+  transition: all 0.3s ease-in-out;
+  font-size: 18px;
+  padding: 5px 15px;
+  background: none;
+  color: #1a3b5d;
+  font-family: "Source Sans Pro", sans-serif;
+}
+.card-form__row {
+  display: flex;
+  align-items: flex-start;
+}
+.card-form__col {
+  flex: auto;
+}
+.card-form__col.-cvv {
+  max-width: 150px;
+}
+.card-form__col:last-child {
+  margin-right: 0;
+}
+.card-form__group {
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+.card-input__input.-select {
+  -webkit-appearance: none;
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAeCAYAAABuUU38AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAUxJREFUeNrM1sEJwkAQBdCsngXPHsQO9O5FS7AAMVYgdqAd2IGCDWgFnryLFQiCZ8EGnJUNimiyM/tnk4HNEAg/8y6ZmMRVqz9eUJvRaSbvutCZ347bXVJy/ZnvTmdJ862Me+hAbZCTs6GHpyUi1tTSvPnqTpoWZPUa7W7ncT3vK4h4zVejy8QzM3WhVUO8ykI6jOxoGA4ig3BLHcNFSCGqGAkig2yqgpEiMsjSfY9LxYQg7L6r0X6wS29YJiYQYecemY+wHrXD1+bklGhpAhBDeu/JfIVGxaAQ9sb8CI+CQSJ+QmJg0Ii/EE2MBiIXooHRQhRCkBhNhBcEhLkwf05ZCG8ICCOpk0MULmvDSY2M8UawIRExLIQIEgHDRoghihgRIgiigBEjgiFATBACAgFgghEwSAAGgoBCBBgYAg5hYKAIFYgHBo6w9RRgAFfy160QuV8NAAAAAElFTkSuQmCC);
+  background-size: 12px;
+  background-position: 90% center;
+  background-repeat: no-repeat;
+}
+.card-form__group .card-input__input:last-child {
+  margin-right: 0;
+}
+.card-list {
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
