b61c5e7bf7c6153e144c792907ff92fe6074cb78
Furkan <frknsprnl@hotmail.com>
2021-12-28 22:27:15 +0300

    welcome screen headertext || backend

--- src/components/Home/WelcomeScreen.vue
+++ src/components/Home/WelcomeScreen.vue
@@ -2,7 +2,7 @@
   <div class="d-flex justify-content-between-sm gradient-border">
     <div class="row-2 mt-4 ms-4">
       <h2 class="col-8 ms-4 mb-5 text-white">
-        Online başvuruya özel avantajları kaçırmamak için Parcell'li ol!
+        {{ homeData.headerText }}
       </h2>
       <router-link class="text-decoration-none" :to="{ name: 'NumberTransfer' }">
         <a class="btn btn-danger btn-lg col-2 mt-5 ms-4"> Parcell'e Geç</a>
@@ -19,6 +19,27 @@ export default {
   components: {
     Logo,
   },
+  data() {
+    return {
+      homeData: {},
+      homeId: "61cb4b95a1a62dc4b20deb43",
+    };
+  },
+  methods: {
+    async getHomeData() {
+      await this.$appAxios
+        .get("/HomePage/GetHomePage/" + this.homeId)
+        .then((response) => {
+          console.log(response.data);
+          this.homeData = response.data;
+          console.log(this.homeData);
+        })
+        .catch((error) => console.error(error));
+    },
+  },
+  async created() {
+    await this.getHomeData();
+  },
 };
 </script>
 
@@ -32,4 +53,3 @@ export default {
 }
 </style>
 
-import axios from 'axios';
