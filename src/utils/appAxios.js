b9b4b11ec052a377b69c362a900298509248268a
Furkan <frknsprnl@hotmail.com>
2022-01-03 19:29:37 +0300

    new deploy

--- src/utils/appAxios.js
+++ src/utils/appAxios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const appAxios = axios.create({
-    baseURL: "https://parcellbackendapi.azure-api.net/v1/api",
-    //baseURL: "https://localhost:44398/api",
-
-}) 
+  baseURL: "https://parcellapibackend.azure-api.net/v1/api",
+  //baseURL: "https://localhost:44398/api",
+});
