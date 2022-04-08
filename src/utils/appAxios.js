3e0ca3ed09642867445bc77d5e7d62be14d6d91a
Muammer Yilmaz <muammercan68@gmail.com>
2022-04-09 00:10:23 +0300

    backend server change

--- src/utils/appAxios.js
+++ src/utils/appAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const appAxios = axios.create({
-  baseURL: "https://parcellapibackend.azure-api.net/v1/api",
+  baseURL: "https://parcell-deneme.herokuapp.com/api",
   //baseURL: "https://localhost:44398/api",
 });
